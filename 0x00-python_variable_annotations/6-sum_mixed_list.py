4d1a7dd7ea9c38c758722c21f1b24b5a15808a8a
Mahmoud Salah <ms685116@gmail.com>
2024-03-14 13:20:29 +0200

    mixed types

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum mixed list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]):
+    """returns sum of mixed list elements"""
+    return sum(mxd_lst)
