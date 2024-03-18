ce22f561b188ba56148c00b3821a409bee048d3f
Mahmoud Salah <ms685116@gmail.com>
2024-03-18 21:21:38 +0200

    concurrent tasks

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""multiple coroutines at the same time"""
+
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n, max_delay):
+    """return the list of all the delays (float values)"""
+    delays = []
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+    return sorted(delays)
